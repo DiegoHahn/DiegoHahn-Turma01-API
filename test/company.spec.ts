bec046ce4f3ef0529e7956109a50cc44c01d7bdc
Diego Hahn <diego-hf@hotmail.com>
2024-10-02 21:35:44 -0300

    subindo mais testes

--- test/company.spec.ts
+++ test/company.spec.ts
@@ -31,6 +31,38 @@ describe('Company API', () => {
         .expectStatus(StatusCodes.CREATED);
     });
   });
+
+  it('Create Company with empty fields', async () => {
+    const invalidRequestBody = {
+      name: '',  
+      cnpj: '',
+      state: '',
+      city: '',
+      address: 'Rua Exemplo, 123',
+      sector: 'Tecnologia'
+    };
+    await p
+      .spec()
+      .post(`${baseUrl}/company`)
+      .withJson(invalidRequestBody)
+      .expectStatus(StatusCodes.BAD_REQUEST);
+  });
+
+  it('Create Company with Invalid CNPJ Format', async () => {
+    const requestBody = {
+      name: faker.word.words(3),          
+      cnpj: 'cnpj',        
+      state: 'SC',
+      city: 'Criciuma',        
+      address: 'Rua Exemplo, 123',
+      sector: 'Tecnologia'    
+    };
+    await p
+      .spec()
+      .post(`${baseUrl}/company`)
+      .withJson(requestBody)
+      .expectStatus(StatusCodes.BAD_REQUEST);
+  });
   
   it('Get All Companies', async () => {
     await p
@@ -58,12 +90,37 @@ describe('Company API', () => {
       .expectStatus(StatusCodes.OK); 
   });
 
+  it('Update Non-existent Company', async () => {
+    const nonExistentCompanyId = 99999;
+    const requestBody = {
+      name: faker.word.words(3),
+      cnpj: '12345678000195',
+      state: 'SC',
+      city: 'Criciuma',
+      address: 'rua exemplo, 123',
+      sector: 'Tecnologia'
+    };
+    await p
+      .spec()
+      .put(`${baseUrl}/company/${nonExistentCompanyId}`)
+      .withJson(requestBody)
+      .expectStatus(StatusCodes.NOT_FOUND);
+  });
+
   it('Get Company by ID', async () => {
     const companyId = 1;
     await p
       .spec()
       .get(`${baseUrl}/company/${companyId}`)
       .expectStatus(StatusCodes.OK)
+  });
+
+  it('Get Company with Invalid ID Format', async () => {
+    const invalidCompanyId = 'letras';
+    await p
+      .spec()
+      .get(`${baseUrl}/company/${invalidCompanyId}`)
+      .expectStatus(StatusCodes.BAD_REQUEST);
   });
 
   it('Delete Company by ID', async () => {
@@ -75,6 +132,15 @@ describe('Company API', () => {
     }
   );
 
+  it('Delete invalid Company', async () => {
+    const companyId = "letras";
+    await p
+      .spec()
+      .delete(`${baseUrl}/company/${companyId}`)
+      .expectStatus(StatusCodes.BAD_REQUEST);
+    }
+  );
+
   it('Get Company by ID - Not Found', async () => {
     const companyId = 999;
     await p
@@ -82,34 +148,4 @@ describe('Company API', () => {
       .get(`${baseUrl}/company/${companyId}`)
       .expectStatus(StatusCodes.NOT_FOUND);
   });
-
-  fit('Add Product to a Company', async () => {
-    const company = {
-      name: faker.word.words(3), 
-      cnpj: '12345678000195',
-      state: 'SC',
-      city: 'Criciuma',
-      address: 'rua exemplo, 123',
-      sector: 'Tecnologia'
-    };
-   
-    const requestBody = {
-      productName: faker.word.words(1),
-      productDescription: faker.word.words(1),
-      price: faker.commerce.price()                       
-    };
-
-    const response =  await p
-      .spec()
-      .post(`${baseUrl}/company`)
-      const companyID = response.body.id;
-
-    await p
-      .spec()
-      .post(`${baseUrl}/company/${companyID}/products`)
-      .withJson(requestBody)
-      .expectStatus(StatusCodes.CREATED)  
-      // .expectBodyContains('productName'); 
-  });
-
 });
